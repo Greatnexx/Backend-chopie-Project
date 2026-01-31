f5333626a61ed2a3c9f50e80d9c76c673156aa6b
Greatnexx <godwindanielgodwin@gmail.com>
2026-01-31 03:50:33 +0100

    last push

--- src/routes/brandingRoutes.js
+++ src/routes/brandingRoutes.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { updateBranding, getBranding } from "../Controllers/brandingController.js";
 import { authenticateToken } from "../middlewares/authMiddleware.js";
-import { uploadLogo } from "../middleware/uploadMiddleware.js";
+import { uploadMenu } from "../middlewares/menuUpload.js";
 
 const router = express.Router();
 
 router.get("/branding", authenticateToken, getBranding);
-router.put("/branding", authenticateToken, uploadLogo.single('logo'), updateBranding);
+router.put("/branding", authenticateToken, uploadMenu.single('logo'), updateBranding);
 
 export default router;
