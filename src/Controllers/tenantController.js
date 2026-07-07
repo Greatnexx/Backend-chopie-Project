e1cd888c6c656ee02098f33f9e923cd936d3daf3
Greatnexx <godwindanielgodwin@gmail.com>
2026-07-08 00:41:26 +0100

    adusted reg flow

--- src/Controllers/tenantController.js
+++ src/Controllers/tenantController.js
@@ -57,7 +57,7 @@ export const registerRestaurant = async (req, res) => {
         "Phone": restaurant.phone || '',
         "Address": restaurant.address || '',
         "Subdomain": restaurant.subdomain,
-        "Menu URL": `https://${restaurant.subdomain}.chopie-resturant-frontend.vercel.app`,
+        "Menu URL": `${process.env.PROD_FRONTEND_URL}/menu/${restaurant.subdomain}`,
         "Dashboard URL": `${process.env.PROD_FRONTEND_URL}/restaurant/login`
       };
       
